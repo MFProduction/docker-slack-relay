a0b37591e2c2e6e94075de6f5b2c8dfd7e47f76d
Jesse Crocker <jesse@gaiagps.com>
2015-11-06 12:30:41 -0700

    add option for a default channel

--- relay.py
+++ relay.py
@@ -7,6 +7,7 @@ from twisted.web.resource import Resource
 from twisted.internet import reactor
 
 url = os.environ['slack_url']
+default_channel = os.environ.get('default_channel', None)
 port = int(os.environ['relay_port'])
 docker_icon_url = 'https://pbs.twimg.com/profile_images/378800000124779041/fbbb494a7eef5f9278c6967b6072ca3e_200x200.png'
 headers = {'content-type': 'application/json'}
@@ -17,7 +18,7 @@ def channel_of_repo_name(repo_name):
     if repo_name in channel_selector:
         return channel_selector[repo_name]
     else:
-        return None
+        return default_channel
 
 def make_slack_post(docker_data):
     repo = docker_data['repository']
